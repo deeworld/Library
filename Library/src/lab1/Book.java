07f26661dd2aa461eb728e845cb792163efa0754
dheyazen <DEE@DEEPC>
2015-05-20 23:12:33 -0400

    library hw finished

--- Library/src/lab1/Book.java
+++ Library/src/lab1/Book.java
@@ -22,6 +22,8 @@ public class Book {
      */
     public Book(String bookTitle) {
         // TODO: Implement this method
+        title = bookTitle;
+        borrowed = false;
     }
    
     /** 
@@ -29,6 +31,7 @@ public class Book {
      */
     public void rented() {
         // TODO: Implement this method
+        borrowed = true;
     }
    
     /**
@@ -36,6 +39,7 @@ public class Book {
      */
     public void returned() {
         // TODO: Implement this method
+        borrowed = false;
     }
    
     /**
@@ -44,6 +48,7 @@ public class Book {
      */
     public boolean isBorrowed() {
         // TODO: Implement this method
+        return borrowed;
     }
    
     /** 
@@ -52,16 +57,17 @@ public class Book {
      */
     public String getTitle() {
         // TODO: Implement this method
+        return title;
     }
 
     public static void main(String[] arguments) {
         // Small test of the Book class
         Book example = new Book("The Da Vinci Code");
         System.out.println("Title (should be The Da Vinci Code): " + example.getTitle());
-        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+        System.out.println("Rented? (should be false): " + example.isBorrowed());
         example.rented();
-        System.out.println("Borrowed? (should be true): " + example.isBorrowed());
+        System.out.println("Rented? (should be true): " + example.isBorrowed());
         example.returned();
-        System.out.println("Borrowed? (should be false): " + example.isBorrowed());
+        System.out.println("Rented? (should be false): " + example.isBorrowed());
     }
 } 
--- Library/src/lab1/Library.java
+++ Library/src/lab1/Library.java
@@ -6,6 +6,78 @@ package lab1;
  */
 public class Library {
     // TODO: Add the missing implementation to this class
+      String address;
+    java.util.ArrayList<Book> books;
+
+    public static final String openTime = "9am";
+    public static final String closeTime = "5pm";
+
+    public Library(String libraryAddress) {
+        address = libraryAddress;
+        books = new java.util.ArrayList<Book>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public static void printOpeningHours() {
+        System.out.println("Libraries are open daily from " + openTime + " to " + closeTime);
+    }
+        
+    public void printAddress() {
+        System.out.println(address);
+    }
+
+    public void printAvailableBooks() {
+        boolean bookPresent = false;
+        for (Book book : books) {
+            if (!book.isBorrowed()) {
+                System.out.println(book.getTitle());
+                bookPresent = true;
+            }
+        }
+        if (!bookPresent) {
+            System.out.println("No book in catalog");
+        }
+    }
+
+    public void borrowBook(String title) {
+        int found = 0;
+        for (Book book : books) {
+            if (book.getTitle().equals(title)) {
+                if (found == 0) {
+                    found = 1;
+                    }
+                if (!book.isBorrowed()) {
+                    book.rented();
+                    found = 2;
+                    break;
+                };
+            }
+        }
+        if (found == 0) {
+            System.out.println("Sorry, this book is not in our catalog.");
+        } else if (found == 1) {
+            System.out.println("Sorry, this book is already borrowed.");
+        } else if (found == 2) {
+            System.out.println("You successfully borrowed " + title);
+        }
+    }
+
+    public void returnBook(String title) {
+        boolean found = false;
+        for (Book book : books) {
+            if (book.getTitle().equals(title) && book.isBorrowed()) {
+                book.returned();
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+           System.out.println("You successfully returned " + title);
+        }
+    }
 
     public static void main(String[] args) {
         // Create two libraries
